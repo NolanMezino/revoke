c7fff230e2ee2fc0944c6ec2c2f51c7615481c23
revoke-me <nolanmezino@gmail.com>
2024-04-08 15:31:16 +0200

    Nuxt clean install

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-images">lol</div>
+</template>
+
+<style lang="scss">
+body {
+  background-color: #000;
+}
+
+.home-images {
+  position: absolute;
+  background-color: #fff;
+  max-width: fit-content;
+  min-width: 150px;
+}
+</style>
+
+<script lang="js"></script>
