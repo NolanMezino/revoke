4dff07269f8195f921a6bd74d4c2618dea242601
revoke-me <nolanmezino@gmail.com>
2024-04-08 21:52:02 +0200

    Home page style beginning

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,48 @@
 <template>
-  <div class="home-images"></div>
+  <H1 class="revoke">revoke</H1>
+  <p class="introduction">
+    To my Alzheimered self:<br />I document what I do, why I do it<br />and how
+    I do it so you can<br />remember who you are.
+  </p>
+  <ul class="home-images">
+    <div id="business"></div>
+    <div id="3d"></div>
+  </ul>
+  <!-- <div class="home-images"></div> -->
 </template>
-
-<script lang="ts" setup>
-const route = useRoute();
-console.log("route name: ", route.name);
-</script>
 
 <style lang="scss">
 body {
+  overflow: hidden;
   background-color: #000;
+}
+.revoke {
+  position: relative;
+  margin-bottom: -180px;
+  margin-top: 0;
+  left: 827px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 180px;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.introduction {
+  position: relative;
+  margin-left: 12px;
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 24px;
+  padding-bottom: 26px;
 }
 
 .home-images {
-  position: absolute;
+  position: relative;
+  left: 840px;
   background-color: #fff;
-  max-width: fit-content;
-  min-width: 150px;
+  min-width: 1020px;
+  max-width: 1020px;
+  min-height: 630px;
+  max-height: 630px;
 }
 </style>
